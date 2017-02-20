f4ceec9682bf70bf0854f9083ba99d1360e21a61
vviimm <wowaonrails@gmail.com>
2017-02-20 15:37:28 +0300

    Fixed after refactoring

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV["MAILER_FROM"] || "no-reply@queens-awards-enterprise.service.gov.uk",
-          reply_to: "queensawards@beis.gov.uk"
+          reply_to: ENV["SUPPORT_EMAIL"]
   layout "mailer"
 end
