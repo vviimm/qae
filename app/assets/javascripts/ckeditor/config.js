b24b7568c53164f16c616a718c71f438e95a14fd
Ruslan Khamidullin <rusllonrails@gmail.com>
2017-04-20 19:58:33 +0300

    [FORM | TEXTAREA FORMATTING] PDF - SOME CODE CLEAN UP AND REFACTORING
    [TRELLO STORY](https://trello.com/c/3OCMPeXD/596-qae17imp-as-an-applicant-i-would-like-to-be-able-to-add-formating-to-my-application)

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,7 +1,6 @@
 CKEDITOR.editorConfig = function (config) {
   config.toolbar_mini = [
     {name: "clipboard", items: ["Cut", "Copy", "PasteText", "-", "Undo", "Redo"]},
-    {name: "links", items: ["Link", "Unlink"]},
     {name: "tools", items: ["Maximize"]},
     {name: "basicstyles", items: ["Bold", "Italic",  "-", "RemoveFormat"]},
     {name: "paragraph", items: ["NumberedList", "BulletedList", "-", "Outdent", "Indent", "-", "JustifyLeft", "JustifyCenter", "JustifyRight", "JustifyBlock"]}
